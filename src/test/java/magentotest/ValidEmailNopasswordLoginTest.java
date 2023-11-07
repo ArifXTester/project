e690fedf84b577bae4e367a07469fb1c05333d7d
Tanvir <tanvirc6@gmail.com>
2023-11-06 23:22:54 -0500

    pkg

--- src/test/java/magentotest/ValidEmailNopasswordLoginTest.java
+++ src/test/java/magentotest/ValidEmailNopasswordLoginTest.java
@@ -1,2 +1,27 @@
-package magentotest;public class ValidEmailNopasswordLoginTest {
+package magentotest;
+
+import base.CommonAPI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.testng.annotations.Test;
+import pages.magento.LoginPage;
+
+public class ValidEmailNopasswordLoginTest extends CommonAPI {
+    Logger LOG = (Logger) LogManager.getLogger(ValidEmailNopasswordLoginTest.class.getName());
+
+    String email = prop.getProperty("magEmail");
+    String password = "";
+
+    LoginPage loginPage = new LoginPage(getDriver());
+
+    @Test
+    public void clickSignIn(){
+        loginPage.clickSignIn();
+    }
+    @Test
+    public void LogIn(){
+        loginPage.email(email);
+        loginPage.password(password);
+        loginPage.SignIn();
+    }
 }
