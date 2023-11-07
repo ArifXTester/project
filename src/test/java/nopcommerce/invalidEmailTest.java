35b7a64a951238c2edcf4c618eadf1f1d4a0aac9
Shahidul Islam <shislam.student@wust.edu>
2023-11-06 23:03:20 -0500

    next

--- src/test/java/nopcommerce/invalidEmailTest.java
+++ src/test/java/nopcommerce/invalidEmailTest.java
@@ -1,2 +1,26 @@
-package nopcommerce;public class invalidEmailTest {
+package nopcommerce;
+
+import base.CommonAPI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import pages.nopcommerce.loginPage;
+
+public class invalidEmailTest extends CommonAPI {
+    Logger LOG = LogManager.getLogger(invalidEmailTest.class.getName());
+    String Email = "";
+    String Password = prop.getProperty("nopPw");
+    loginPage loginPage = new loginPage(getDriver());
+    @Test
+    public void ClickLogin(){
+        loginPage.ClickLogIn();
+    }
+    public void fillForm(){
+        loginPage.SelectEmail(Email);
+        loginPage.SelectPassword(Password);
+        loginPage.ClickLogIn();
+    }
+
+
+
 }
